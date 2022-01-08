ab3a369e56819f6bdf9ea33838d129b546197e11
Sander Wieringa <s.wieringa@student.fontys.nl>
2022-01-08 15:00:25 +0100

    fresh start

--- Dijkstra/src/main/java/Main.java
+++ Dijkstra/src/main/java/Main.java
@@ -5,43 +5,21 @@ public class Main {
     public static void main(String[] args)
     {
 
-                                     //0  1  2
-        int[][] graph = new int[][] { {0, 10, 20},  //0
-                                      {10, 0, 5},   //1
-                                      {20, 5, 0} }; //2
-
-        int v = graph.length;
-        System.out.println("v: " + v);
-
-        Calculator calculator = new Calculator();
-        calculator.calculate(graph, 0);
-    }
-}
-
-class Calculator {
-
-    public void calculate(int[][] graph, int source) {
-        int indexNull = 0;
-        int indexOne = 1;
-        int indexTwo = 2;
-
-            for (int y = 0; y < graph.length; y++) {
-                int vertex = 0;
-                int distance = 0;
-                if ()
-                if (distance < graph[source][y]) {
-                    distance += graph[source][y];
-                    System.out.println(source + " : " + y + " : "  + "distance: " + distance);
-                }
-            }
-
-//            for (int y = 0; y < graph.length; y++) {
-//
-//                System.out.println(graph[i][y]);
-//            }
+                                     //0  1  2   3  4   5   6  7  8
+        int[][] graph = new int[][] { {0, 4, 0,  0, 0,  0,  0, 8, 0},  //0
+                                      {0, 0, 12, 0, 0,  0,  0, 0, 0},   //1
+                                      {0, 4, 19, 0, 0,  0,  0, 0, 14},  //2
+                                      {0, 0, 12, 0, 0,  0,  0, 0, 0},   //3
+                                      {0, 0, 0,  0, 0,  21, 0, 0, 0},   //4
+                                      {0, 0, 0,  0, 21, 0,  9, 0, 0},   //5
+                                      {0, 0, 0,  0, 0,  11, 0, 8, 0},   //6
+                                      {0, 0, 0,  0, 0,  0,  9, 0, 0},   //7
+                                      {0, 0, 12, 0, 0,  0,  0, 0, 0},}; //8
 
     }
 }
+
+
 
 
 
